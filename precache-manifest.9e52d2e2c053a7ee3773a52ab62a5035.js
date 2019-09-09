2ea01aae44e05f6df102dc5dbfaa882dde25445c
Sofiqul Islam <schoolwab@gmail.com>
2019-09-09 06:14:48 +0600

    Updates

---
+++ precache-manifest.9e52d2e2c053a7ee3773a52ab62a5035.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fece47344de7b084bcff383b4d5ec9",
+    "url": "/notebook/index.html"
+  },
+  {
+    "revision": "7e8317dead5753deb105",
+    "url": "/notebook/static/css/main.58876261.chunk.css"
+  },
+  {
+    "revision": "6ee6770f5c4ff1123224",
+    "url": "/notebook/static/js/2.6f625f51.chunk.js"
+  },
+  {
+    "revision": "7e8317dead5753deb105",
+    "url": "/notebook/static/js/main.89e545e0.chunk.js"
+  },
+  {
+    "revision": "84537e1341c3761792e5",
+    "url": "/notebook/static/js/runtime~main.1059fa5c.js"
+  }
+]);
